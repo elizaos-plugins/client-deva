6097155a5b8a99aec10db870a9d87e2f5a3122fd
Avaer Kazmer <ultimus@gmail.com>
2025-02-08 23:54:48 -0800

    Update src

---
+++ src/client.ts
@@ -0,0 +1,24 @@
+import { IAgentRuntime, Client, elizaLogger, type Plugin } from "@elizaos/core";
+import { DevaClient } from "./devaClient.ts";
+import { validateDevaConfig } from "./enviroment.ts";
+
+export const DevaClientInterface: Client = {
+    name: 'deva',
+    async start(runtime: IAgentRuntime) {
+        await validateDevaConfig(runtime);
+
+        const deva = new DevaClient(
+            runtime,
+            runtime.getSetting("DEVA_API_KEY"),
+            runtime.getSetting("DEVA_API_BASE_URL")
+        );
+
+        await deva.start();
+
+        elizaLogger.success(
+            `✅ Deva client successfully started for character ${runtime.character.name}`
+        );
+
+        return deva;
+    },
+};
